563dc1b0211550a84e5c91d46b7580e1345c8b7f
Aleksey Shugaev <alek-2020@yandex.ru>
2020-12-28 22:46:35 +0300

    chore: rm log message

--- src/helpers/wait.ts
+++ src/helpers/wait.ts
@@ -5,7 +5,5 @@ export const wait = (minTime: number, maxTime?: number) => new Promise((rs) => {
     minTime = Math.floor(Math.random() * (maxTime - minTime) + minTime);
   }
 
-  log.debug('wait', minTime, 'milliseconds');
-
   setTimeout(rs, minTime);
 });
