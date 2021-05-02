3cc59582cf6266336558c2e8b5470a65e218ce04
Aleksei Shugaev <a.shugaev@admitad.com>
2021-05-02 15:21:18 +0300

    feat: автоматическое обновление базы доступных инструментов

---
+++ src/models/InstrumentsList.ts
@@ -0,0 +1,44 @@
+import { prop, getModelForClass } from '@typegoose/typegoose'
+import { ICoingecoSpecificBaseData } from "../marketApi/coingecko/types";
+import { ITinkoffSpecificBaseData } from "../marketApi/tinkoff/types";
+import { EMarketDataSources, EMarketInstrumentTypes, IBaseInstrumentData } from "../marketApi/types";
+
+export class InstrumentsList {
+    @prop({ required: true, unique: true })
+    ticker: string
+
+    @prop({ required: true })
+    name: string
+
+    @prop({ required: true })
+    source: EMarketDataSources
+
+    @prop({ required: true })
+    type: EMarketInstrumentTypes
+
+    @prop({ required: true })
+    sourceSpecificData: ICoingecoSpecificBaseData | ITinkoffSpecificBaseData
+}
+
+const InstrumentsListModel = getModelForClass(InstrumentsList, {
+    schemaOptions: { timestamps: true },
+    options: {
+        customName: 'instrumentslist'
+    }
+})
+
+export async function clearInstrumentsList() {
+    try {
+        await InstrumentsListModel.deleteMany({});
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+export async function putItemsToInstrumentsList(items: IBaseInstrumentData[]) {
+    try {
+        await InstrumentsListModel.insertMany(items)
+    } catch (e) {
+        throw new Error(e);
+    }
+}
