c68a416d66b594e2f87efadda534ec6c22c45275
Aleksei Shugaev <a.shugaev@admitad.com>
2021-05-02 14:15:47 +0300

    feat: исключение дубликатов в списке инструментов

--- src/marketApi/index.ts
+++ src/marketApi/index.ts
@@ -2,10 +2,20 @@ import { coingeckoGetAllInstruments } from "./coingecko/api/getAllInstruments";
 import { tinkoffGetAllInstruments } from "./tinkoff/api/getAllInstruments";
 import { IBaseInstrumentData } from "./types";
 
+const excludeInstrumentDuplicates = (items): IBaseInstrumentData[] => {
+    const itemsObj = items.reduce((acc, item) => {
+        acc[item.ticker] = item
+
+        return acc;
+    }, {})
+
+    return Object.values(itemsObj);
+}
+
 /**
  * Получает список всех инструментов подключенных к боту
  */
-export const getAllInstruments = async ():Promise<IBaseInstrumentData[]> => {
+export const getAllInstruments = async (): Promise<IBaseInstrumentData[]> => {
     const allInstrumentsPromises = [
         tinkoffGetAllInstruments(),
         coingeckoGetAllInstruments(),
@@ -13,5 +23,5 @@ export const getAllInstruments = async ():Promise<IBaseInstrumentData[]> => {
 
     const allInstruments = await Promise.all(allInstrumentsPromises);
 
-    return allInstruments;
+    return excludeInstrumentDuplicates(allInstruments);
 }
