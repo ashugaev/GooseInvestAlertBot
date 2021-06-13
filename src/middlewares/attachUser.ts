d09434e398cfbcde63e507d73c602ea42f261b5a
Aleksei Shugaev <a.shugaev@admitad.com>
2021-06-13 18:19:49 +0300

    доп инфа при логгировании ошибки отсутствия  id

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -1,9 +1,18 @@
-// Dependencies
+import { log } from "../helpers/log";
 import { findUser } from '../models'
 import { Context } from 'telegraf'
 
 export async function attachUser(ctx: Context, next) {
-  const dbuser = await findUser(ctx.from.id)
+  const userId = ctx.from?.id;
+
+  // FIXME: Это костыль, который пропустит неавторизованного юзера в систему (может это бот. Не понятно)
+  if(!userId) {
+    log.error('no user id from obj', ctx.from);
+
+    next()
+  }
+
+  const dbuser = await findUser(userId)
   // @ts-ignore
   ctx.dbuser = dbuser
   next()
