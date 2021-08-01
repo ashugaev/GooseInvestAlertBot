44d9acef7524beea4db0a9f5f1737ff27f4348fe
Aleksei Shugaev <a.shugaev@admitad.com>
2021-08-01 13:10:05 +0300

    правки конфига линта

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true // Allows for the parsing of JSX
     },
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
