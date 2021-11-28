e62f2b0ee23dd39b6072762002fad84eed094c0f
Aleksey Shugaev <alekseyshugaev@Alekseys-MacBook-Pro.local>
2021-11-28 17:24:44 +0300

    in progress

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Проект сделан на основе [telegraf-template](https://github.com/backmeupplz/telegraf-template)
 
 Вопросы по боту можно писать сюда: https://t.me/GooseInvestAlertChat
+
+TODO:
+- Пагинация 
+- Перетерка тикеров
+- Дебаг сообщения для шифтов
--- src/marketApi/index.ts
+++ src/marketApi/index.ts
@@ -2,16 +2,6 @@ import { coingeckoGetAllInstruments } from './coingecko/api/getAllInstruments'
 import { tinkoffGetAllInstruments } from './tinkoff/api/getAllInstruments'
 import { IBaseInstrumentData } from './types'
 
-const excludeInstrumentDuplicates = (items): IBaseInstrumentData[] => {
-  const itemsObj = items.reduce((acc, item) => {
-    acc[item.ticker] = item
-
-    return acc
-  }, {})
-
-  return Object.values(itemsObj)
-}
-
 /**
  * Получает список всех инструментов подключенных к боту
  */
@@ -25,5 +15,5 @@ export const getAllInstruments = async (): Promise<IBaseInstrumentData[]> => {
 
   const allInstruments = allInstrumentsArr.reduce((acc, el) => acc.concat(el), [])
 
-  return excludeInstrumentDuplicates(allInstruments)
+  return allInstruments;
 }
