b03813bf67b343cff7b1dfe5b02f9ac0a99c17b4
Aleksei Shugaev <yashugaev@gmail.com>
2023-02-02 13:39:59 +0700

    fix menu for pay

--- src/commands/pay/pay.keyboards.ts
+++ src/commands/pay/pay.keyboards.ts
@@ -1,6 +1,6 @@
 import { TARIFFS } from './pay.constants'
 
-export const payPricesKeyboard = [...TARIFFS.map(tariff => ({
+export const payPricesKeyboard = [...TARIFFS.map(tariff => ([{
   text: tariff.buttonText,
   callback_data: tariff.price
-}))]
+}]))]
