96f29838ca1d5aa6306b8bbe2bd3243674cc28f1
Aleksei Shugaev <yashugaev@gmail.com>
2023-02-02 14:40:49 +0700

    generate payment link

--- src/commands/pay/pay.keyboards.ts
+++ src/commands/pay/pay.keyboards.ts
@@ -1,6 +1,11 @@
-import { TARIFFS } from './pay.constants'
+import { createActionString } from '@helpers'
+import { Markup } from 'telegraf'
 
-export const payPricesKeyboard = [...TARIFFS.map(tariff => ([{
-  text: tariff.buttonText,
-  callback_data: tariff.price
-}]))]
+import { PAY_ACTIONS, TARIFFS } from './pay.constants'
+
+export const payPricesKeyboard = [...TARIFFS.map((tariff, i) => ([
+  Markup.callbackButton(
+    tariff.buttonText,
+    createActionString(PAY_ACTIONS.generatePaymentLink, { i })
+  )
+]))]
