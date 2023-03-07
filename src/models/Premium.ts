0d5c71c6e9a8b8f5c9776c1f256d415f15716f2a
Aleksei Shugaev <yashugaev@gmail.com>
2023-03-07 15:16:41 +0700

    premium models

---
+++ src/models/Premium.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line
+import {getModelForClass, prop} from '@typegoose/typegoose'
+
+/**
+ * Данные о подписке на бота
+ */
+export class Premium {
+  @prop({ required: true, unique: false })
+  userId: number
+
+  @prop({ required: true})
+  end: Date
+
+  @prop({ required: true})
+  start: Date
+
+  @prop({ required: false})
+  isTrial: boolean
+
+  /**
+   * Откуда пришел юзер, какие-то теги или еще что-то
+   */
+  @prop({ required: false})
+  marketingSource: string
+}
+
+export const PremiumModel = getModelForClass(Premium, {
+  schemaOptions: { timestamps: true }
+})
