b0cfd362b4f51ab2b787a2763a305cc70ed13803
Aleksei Shugaev <yashugaev@gmail.com>
2023-04-23 19:42:27 +0400

    fix

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,14 +8,4 @@ export function setupHelp (bot: Telegraf<Context>) {
       { disable_web_page_preview: true }
     )
   }))
-
-  // Handle bot invite
-  bot.on('new_chat_members', (ctx) => {
-    ctx.replyWithHTML('Привет! Я бот для управления сменами и ценами в баре. Напиши /help для получения списка команд.')
-  })
-
-  // Handle bot kick from the chat
-  bot.on('left_chat_member', (ctx) => {
-    ctx.replyWithHTML('Привет! Я бот для управления сменами и ценами в баре. Напиши /help для получения списка команд.')
-  })
 }
