a7a8f3dcad2bbeb457e64744a4f5a47f1ab4b089
Aleksei Shugaev <yashugaev@gmail.com>
2023-04-23 19:46:13 +0400

    fix

--- src/helpers/commandWrapper.ts
+++ src/helpers/commandWrapper.ts
@@ -9,25 +9,25 @@ interface CommandWrapperConfig {
 }
 
 export function commandWrapper({availableForAdmins, availableForUsers = true}: CommandWrapperConfig, callback: (ctx: Context) => Promise<void>): Middleware<TelegrafContext> {
-    return async (ctx) => {
-        if (!availableForAdmins && ctx.dbuser.adminMode) {
-            try {
-                await ctx.replyWithHTML(ctx.i18n.t('adminMode_commandHidden'))
-            } catch (e) {
-                log.error('Error in wrapper:', e)
-            }
-            return
-        }
-
-        if(!availableForUsers && !ctx.dbuser.adminMode) {
-            return
-        }
-
-        try {
-            await callback(ctx)
-        } catch (e) {
-            await ctx.replyWithHTML(ctx.i18n.t('unrecognizedError'))
-            log.error(e)
-        }
+  return async (ctx) => {
+    if (!availableForAdmins && ctx.dbuser.adminMode) {
+      try {
+        await ctx.replyWithHTML(ctx.i18n.t('adminMode_commandHidden'))
+      } catch (e) {
+        log.error('Error in wrapper:', e)
+      }
+      return
     }
+
+    if(!availableForUsers && !ctx.dbuser.adminMode) {
+      return
+    }
+
+    try {
+      await callback(ctx)
+    } catch (e) {
+      await ctx.replyWithHTML(ctx.i18n.t('unrecognizedError'))
+      log.error(e)
+    }
+  }
 }
