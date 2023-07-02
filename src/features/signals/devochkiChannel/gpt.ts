ca677aa43a9926c6532c342e5fa94456a4b32a27
Aleksei Shugaev <yashugaev@gmail.com>
2023-07-02 12:10:56 +0100

    validation message

---
+++ src/features/signals/devochkiChannel/gpt.ts
@@ -0,0 +1,73 @@
+import { logPrefixDevochki } from '@/features/signals/devochkiChannel/devochkiChannel.constants'
+import { log } from '@/helpers'
+import { openai } from '@/integrations/openai'
+
+export const validateWithChatGPT = async (message) => {
+  try {
+    const chat_completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      max_tokens: 50,
+      temperature: 0,
+      messages: [
+        {
+          role: 'system',
+          content: `
+        Извлеки данные из сообщений в указанном формате:
+        [[ticker:string|null], [ценаЗакрепления:number|null], [целиДляТейкПрофита:number|number[]|null], [стопЛоссВПроцентах:number|null], [типОрдера:buy|sell|null], [сомненияВСделке:yes|no]]
+        целиДляТейкПрофита - если тут указан, диапазон, то выбирай наименьшее число
+        Все строки должны быть обернуты в одинарные кавычки результатом должен быть валидный массив
+        Для дробных чисел используй точку
+        Примеры валидного ответа: 
+          ['LINA', 0.01420, [2, 4, 6, 8], 2.5, 'sell', 'yes'], 
+          ['BTC', 0.01420, null, 2.5, 'buy', 'yes'], 
+          ['BTC', 0.01420, [2], null, null, 'no'], 
+          [null, null, [2, 4, 6, 8], null, 'sell', 'no']
+      `,
+        },
+        {
+          role: 'user',
+          content:
+            '#APE - при закреплении на уровне 2,2 - лонг Цели первые 2-4%, далее, сели продолжится рост, 6-8% стоп 2%',
+        },
+        {
+          role: 'assistant',
+          content: "['APE', 2.2, [2, 6], 2, 'buy', 'no']",
+        },
+        {
+          role: 'user',
+          content:
+            '#JASMY - сделка с повышенным риском на текущем рынке❗️ При закреплении на уровне 0,0047 - лонг цели 2-4-6% стоп 2% Кто не готов к риску, лучше пропустить.',
+        },
+        {
+          role: 'assistant',
+          content: "['JASMY', 0.0047, [2, 4, 6], 2, 'buy', 'yes']",
+        },
+        {
+          role: 'user',
+          content: message,
+        },
+      ],
+    })
+
+    if (chat_completion.status === 200) {
+      const answer = chat_completion.data.choices?.[0]?.message?.content
+      if (answer.length) {
+        return answer
+      } else {
+        log.error(logPrefixDevochki, 'Chat GPT answer is empty')
+        return null
+      }
+    } else {
+      log.error(
+        logPrefixDevochki,
+        'Not 200 status from chat GPT',
+        chat_completion.statusText
+      )
+      return null
+    }
+  } catch (e) {
+    debugger
+
+    console.log(e)
+  }
+}
--- src/features/signals/devochkiChannel/handleMessage.ts
+++ src/features/signals/devochkiChannel/handleMessage.ts
@@ -1,15 +1,85 @@
+import { logPrefixDevochki } from '@/features/signals/devochkiChannel/devochkiChannel.constants'
+import { validateWithChatGPT } from '@/features/signals/devochkiChannel/gpt'
+import { log } from '@/helpers'
+import { SignalModel } from '@/models/Signal'
 import { TrackChatCallbacksParams } from '@/models/TrackChat'
 
 export const handleDevochkiChannelMessage = async (
   data: TrackChatCallbacksParams
 ) => {
-  // Manual validation
-  // Detect message type
-  // Type 1: Signal
-  // Type 2: Move stop
-  // Chat GPT validation
-  // Validation of chat GPT answer
-  // Смотрим что уже нет активной сделки по этому тикеру
-  // Make orders
-  // Ожидание закрепления. Отправим с определнным статусом джобу в очередь
+  const pattern1 = /#[A-Z]{3,6}/ // ticker
+  const pattern2 = /\d+%/ // number
+  const pattern3 = /закреп/ // закреп notice
+  const pattern4 = /(лонг|шорт)/ // type
+
+  const signalItem = new SignalModel({
+    message: data.message,
+    messageId: data.messageId,
+    channel: data.chatLinkName,
+    validationStatus: 'notStarted',
+  })
+
+  try {
+    if (
+      !pattern1.test(data.message) ||
+      !pattern2.test(data.message) ||
+      !pattern3.test(data.message) ||
+      !pattern4.test(data.message) ||
+      data.message.length <= 40
+    ) {
+      signalItem.validationStatus = 'manualCheckFailed'
+      throw 'Message is not valid'
+      return
+    }
+
+    const AIRes = await validateWithChatGPT(data.message)
+
+    if (!AIRes) {
+      signalItem.validationStatus = 'aiCheckFailed'
+      throw 'AI check failed'
+    }
+
+    signalItem.chatGptValidationMessage = AIRes
+
+    let dataArr = null
+
+    try {
+      dataArr = JSON.parse(AIRes.replace(/'/g, '"'))
+    } catch (e) {
+      signalItem.validationStatus = 'aiAnswerInvalid'
+      throw 'AI answer json is invalid'
+    }
+
+    const [ticker, level, tp, stop, type, doubts] = dataArr
+
+    // Validate answer
+    if (
+      !ticker.length ||
+      !(level > 0) ||
+      !tp.length ||
+      !tp.every((el) => el > 0) ||
+      !(stop > 0) ||
+      !type.match(/(buy|sell)/) ||
+      !doubts.match(/(yes|no)/)
+    ) {
+      signalItem.validationStatus = 'aiAnswerInvalid'
+      throw 'AI answer is invalid'
+    }
+
+    signalItem.doubts = doubts
+    signalItem.type = type
+
+    // Get ticker info
+    // get price
+    // check how much to go untill level
+    // Set limit or market order depending on price
+    // Смотрим что уже нет активной сделки по этому тикеру
+    // Make orders
+    // Ожидание закрепления. Отправим с определнным статусом джобу в очередь
+    // Stops updater start
+  } catch (e) {
+    log.info(logPrefixDevochki, e)
+  } finally {
+    signalItem.save()
+  }
 }
