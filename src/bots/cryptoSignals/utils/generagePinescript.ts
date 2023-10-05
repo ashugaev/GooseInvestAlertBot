4b986013cedf54c0fb3dfc155c7d281c59500aaf
Aleksei Shugaev <yashugaev@gmail.com>
2023-10-05 15:09:22 +0100

    pine master

--- src/bots/cryptoSignals/utils/generagePinescript.ts
+++ src/bots/cryptoSignals/utils/generagePinescript.ts
@@ -21,30 +21,65 @@ type PointWithLabel = {
   timestamp: number // Unix timestamp in milliseconds
   price: number
   label: string
-  labelColor: PineColor // New parameter for label color
+  labelColor: PineColor
 }
 
-function generatePineScriptCode(point: PointWithLabel): string {
+function generatePineScriptCode(points: PointWithLabel[]): string {
+  const codeForPoints = points
+    .map((point) => {
+      const roundedTimestamp = `floor(${point.timestamp} / currentResolution) * currentResolution`
+      return `
+isBarForPoint${point.label.replace(/\s/g, '')} = time == ${roundedTimestamp}
+plotshape(series=isBarForPoint${point.label.replace(/\s/g, '')} ? ${
+        point.price
+      } : na, style=shape.circle, location=location.absolute, color=${
+        point.labelColor
+      }, title="Point")
+if (isBarForPoint${point.label.replace(/\s/g, '')})
+    label.new(x=bar_index, y=${point.price} + syminfo.mintick * 2, text="${
+        point.label
+      }", style=label.style_labeldown, color=${
+        point.labelColor
+      }, xloc=xloc.bar_index, yloc=yloc.price)
+    `
+    })
+    .join('\n')
+
   return `
 //@version=4
-study(title="Point with Label", overlay=true)
+study(title="Multiple Points with Labels", overlay=true)
 
 currentResolution = abs(time[1] - time)
-roundedTimestamp = floor(${point.timestamp} / currentResolution) * currentResolution
-
-isBarForPoint = time == roundedTimestamp
-
-plotshape(series=isBarForPoint ? ${point.price} : na, style=shape.circle, location=location.absolute, color=color.black, title="Point")
-if (isBarForPoint)
-    label.new(x=bar_index, y=${point.price}  + syminfo.mintick * 2, text="${point.label}", style=label.style_labeldown, color=${point.labelColor}, xloc=xloc.bar_index, yloc=yloc.price)
-`
+${codeForPoints}
+  `
 }
 
 // Пример использования:
-const pointData: PointWithLabel = {
-  timestamp: new Date().getTime(), // Ensure this timestamp is in milliseconds
-  price: 0.793,
-  label: 'My Point',
-  labelColor: 'color.navy', // Passing color as a Pine Script color string
-}
-console.log(generatePineScriptCode(pointData))
+const pointsData: PointWithLabel[] = [
+  {
+    timestamp: new Date().getTime(),
+    price: 0.793,
+    label: 'PointA',
+    labelColor: 'color.blue',
+  },
+  {
+    timestamp: new Date().getTime() + 60000, // +1 minute
+    price: 0.795,
+    label: 'PointB',
+    labelColor: 'color.red',
+  },
+  {
+    timestamp: new Date().getTime(),
+    price: 0.793,
+    label: 'PointC',
+    labelColor: 'color.aqua',
+  },
+  {
+    timestamp: new Date().getTime() + 1200000, // +1 minute
+    price: 0.795,
+    label: 'PointD',
+    labelColor: 'color.green',
+  },
+]
+
+console.log(generatePineScriptCode(pointsData))
